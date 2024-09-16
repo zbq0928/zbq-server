c88c321f2a1ad2a98e8debb3953aafffd994abfa
__ZBQ__ <x_123_d@163.com>
2024-09-16 17:14:05 +0800

    load_screen 80% --> 100%

--- scripts/load_screen.js
+++ scripts/load_screen.js
@@ -1,6 +1,12 @@
+window.onload = load_screen;
 function load_screen() {
     e = document.getElementById("load_screen");
     e.style.opacity = "0";
     e.style.display = "none";
 }
-window.onload = load_screen;
+setTimeout(function() {
+    e = document.getElementById("load_screen");
+    e.style.opacity = "0";
+    setTimeout(function() {
+        e.style.display = "none";},300);
+    },1200);
